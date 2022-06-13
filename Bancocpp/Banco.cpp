4b2da9c0fd603a18bd4d7fca0079b2f406f26e1a
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-06-13 16:29:24 -0300

    Criando funções para operações de saque e depósito

--- Bancocpp/Banco.cpp
+++ Bancocpp/Banco.cpp
@@ -11,6 +11,42 @@ struct conta
 	float Saldo;
 };
 
+void saque(conta& Conta, float ValorASacar)
+{
+	if (ValorASacar <= 0)
+	{
+		cout << "Voce nao pode sacar valores negativos!!" << endl;
+		cout << "Seu saldo é de R$" << Conta.Saldo << endl;
+		return;
+		
+	}
+	else if (ValorASacar > Conta.Saldo)
+	{
+		
+		cout << "Voce nao pode sacar valores maiores do que o seu saldo!!!" << endl;
+		cout << "Seu saldo é de R$" << Conta.Saldo << endl;
+		return;
+	}
+	else
+	{
+		Conta.Saldo -= ValorASacar;
+		cout << "Seu saldo  agora e de R$" << Conta.Saldo << endl;
+
+ 	}
+}
+void depositar(conta& Conta, float ValorADepositar) 
+{
+    if (ValorADepositar<=0) 
+	{
+		cout << "Voce nao pode depositar valores negativos!!" << endl;
+	}
+	else
+	{
+		Conta.Saldo += ValorADepositar;
+		cout << "O Saldo da conta de " << Conta.NomeTitular << " e de R$" << Conta.Saldo << endl; 
+	}
+}
+
 int main()
 {
 	conta Contaum;
@@ -28,5 +64,8 @@ int main()
 	Contadois.Saldo = 100;
 
 	cout << "A conta de " << Contadois.NomeTitular << " tem R$" << Contadois.Saldo << endl;
+	 saque(Contaum,5);
+	 depositar(Contaum, 200);
+
 	return 0;
 }
