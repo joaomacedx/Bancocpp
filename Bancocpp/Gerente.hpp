4e60c5de21cf12b97ba0d4a9deccce3c1cc54f7e
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-07-22 15:22:32 -0300

    Analisando os problemas que a  Herança multipla pode trazer como: chamadas duplicadas do construtor, desperdício de recurso entre outros.

--- Bancocpp/Gerente.hpp
+++ Bancocpp/Gerente.hpp
@@ -3,6 +3,7 @@
 #include "Funcionario.hpp"
 class Gerente final : public Funcionario, public Autenticacao
 {
+
 public:
 	Gerente(Cpf cpf, std::string Nome, float Salario, std::string Senha);
 	float bonificacao() const;
